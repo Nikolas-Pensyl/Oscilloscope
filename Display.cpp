ca8f7b7c23d63e41a9f28e64e1b16a132a7bfc3d
CadenDaffron <cjdaffron@gmail.com>
2023-10-09 15:00:07 -0400

    Working on display clear and initialization

--- Display.cpp
+++ Display.cpp
@@ -44,23 +44,30 @@ void Display::init(SPI_HandleTypeDef hspi1) {
 void Display::clearScreen(SPI_HandleTypeDef hspi1) {
 	uint8_t clear = 0x00;
 	uint8_t page_command = 0xB0;
-	uint8_t scroll_command = 0x40;
-	uint16_t column_command_1 = 0x
+	uint8_t column_commands[2] = {0x00, 0x10};
 
-	// Ready the DOG to receive commands
-	HAL_GPIO_WritePin(GPIOA, DOG_CD_Pin, GPIO_PIN_RESET);
-
-	HAL_SPI_Transmit(&hspi1, (uint8_t*) &scroll_command, 1, HAL_MAX_DELAY);
-	HAL_SPI_Transmit(&hspi1, (uint8_t*) &column_command_1, 1, HAL_MAX_DELAY);
-
-
-	for (int i = 0; i <= DOG_MAX_SCROLL_LINE; i++)
+	for (int i = 0; i <= DOG_MAX_PAGE; i++)
 	{
-		for (int j = 0; j < DOG_MAX_COLUMN/8; j++) {
-			HAL_SPI_Transmit(&hspi1, (uint8_t*) &clear, 1, HAL_MAX_DELAY);
+		// Set the CD line low, to signal incoming commands
+		HAL_GPIO_WritePin(GPIOA, DOG_CD_Pin, GPIO_PIN_RESET);
 
+		// Advance the current page
+		HAL_SPI_Transmit(&hspi1, (uint8_t*) &page_command, 1, HAL_MAX_DELAY);
+
+		// Reset to column 0 to begin writing data
+		HAL_SPI_Transmit(&hspi1, (uint8_t*) &column_command[0], 1, HAL_MAX_DELAY);
+		HAL_SPI_Transmit(&hspi1, (uint8_t*) &column_command[1], 1, HAL_MAX_DELAY);
+
+		// Set the CD line high, to signal incoming data
+		HAL_GPIO_WritePin(GPIOA, DOG_CD_Pin, GPIO_PIN_SET);
+
+		// Send clear bytes for each column until
+		for (int i = 0; i <= DOG_MAX_COLUMN; i++)
+		{
+			HAL_SPI_Transmit(&hspi1, (uint8_t*) &clear, 1, HAL_MAX_DELAY);
 		}
 
+		(&page_command)++;
 	}
 }
 
